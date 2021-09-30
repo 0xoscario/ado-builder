a8a37df3e11060b9e5d817c90aa960d4f782f2e0
Leif A Sorensen <60895101+leifasorensen@users.noreply.github.com>
2021-09-30 10:00:50 -0500

    Merging From Original Style Changes
    Conversion of Pull Request #2 to fit into branch. Added override on form field bg to remove black form fields..

--- src/components/login/LoginBox.js
+++ src/components/login/LoginBox.js
@@ -2,70 +2,126 @@
 import React from 'react'
 
 //Load Terra-Money Modules
-import {NetworkInfo, WalletProvider, ConnectType, useWallet, WalletStatus} from '@terra-money/wallet-provider'
-
+import {
+	NetworkInfo,
+	WalletProvider,
+	ConnectType,
+	useWallet,
+	WalletStatus,
+} from '@terra-money/wallet-provider'
 
 const LoginBox = (props) => {
-    const {status, network, wallets, availableConnectTypes, availableInstallTypes, connect, install, disconnect} = useWallet()
+	const {
+		status,
+		network,
+		wallets,
+		availableConnectTypes,
+		availableInstallTypes,
+		connect,
+		install,
+		disconnect,
+	} = useWallet()
 
-    return(
-        <div id="wrapper" className="col-8 offset-lg-2">
-            <div id="loginbox" className="light-opacity rounded-lg m-5 p-5">
-                <div id="login-header" className="text-center text-light">
-                    <img src="./images/adp-logo_labeled.png" className="m-2" height="90" />
-                    <p className="h2">Functional Smart Contracts</p>
-                    <div className="">
-                        <p>NFTs and Contracts embeded with governance, rules, royalties, and other commercial terms</p>
-                    </div>
-                </div>
+	return (
+		<div id="wrapper" className="col-8 offset-lg-2">
+			<div id="loginbox" className="m-5 p-5">
+				<div
+					id="login-header"
+					className="text-center text-light mb-4"
+				>
+					<img
+						src="./images/AND_Logo.svg"
+						className="mb-4"
+						height="50"
+					/>
+					<p className="h6 mt-5">Functional Smart Contracts</p>
+					<div className="">
+						<p
+							className="font-weight-light d-inline-block"
+							style={{ maxWidth: '400px' }}
+						>
+							NFTs and Contracts embeded with governance,
+							rules, royalties, and other commercial terms
+						</p>
+					</div>
+				</div>
 
-                <div id="wrapper-buttons" className="col-10 offset-lg-2">
-                    <div id="extension-button-box" className="row">                        
-                        <div className="col justify-content-center">
-                            {availableInstallTypes.length?
-                                <button
-                                className="btn btn-primary w-75 mt-3 mb-4 p-3"
-                                onClick={() => {
-                                    install('CHROME_EXTENSION')
-                                }}
-                                >
-                                    <span className="float-left mt-2">Install Terra Station Extension</span>
-                                    <img src="./images/connect-icons_extension.png" width="42" className="float-right" />
-                                </button>
-                            :
-                                status === WalletStatus.WALLET_NOT_CONNECTED && (
-                                    <button
-                                    className="btn btn-primary w-75 mt-3 mb-4 p-3"
-                                    onClick={() => {
-                                        connect('CHROME_EXTENSION')
-                                        //props.setShowDashboard(true)
-                                    }}
-                                    >
-                                        <span className="float-left mt-2">Connect Terra Station Extension</span>
-                                        <img src="./images/connect-icons_extension.png" width="42" className="float-right" />
-                                    </button>
-                                )
-                            }
-                        </div>
-                    </div>
-                        
-                    <div id="mobile-button-box" className="row">
-                        <div className="col justify-content-center">
-                        <button
-                                className="btn btn-primary w-75 p-3"
-                                onClick={() => {
-                                    connect('WALLETCONNECT')
-                                }}
-                                >
-                                    <span className="float-left mt-2">Connect Terra Station Mobile</span>
-                                    <img src="./images/connect-icons_mobile.png" width="42" className="float-right" />
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
+				<div id="wrapper-buttons" className="col-12 offset-lg-3">
+					<div id="extension-button-box" className="row">
+						<div className="col justify-content-center">
+							{availableInstallTypes.length ? (
+								<button
+									style={{ height: '60px' }}
+									className="btn btn-primary w-50 mt-3 mb-4 text-left"
+									onClick={() => {
+										install('CHROME_EXTENSION')
+									}}
+								>
+									<img
+										src="./images/terra.svg"
+										width="30"
+										className="ml-4 mr-4"
+										alt="Terra"
+									/>
+									<span className="">
+										Install Terra Station
+										Extension
+									</span>
+								</button>
+							) : (
+								status ===
+									WalletStatus.WALLET_NOT_CONNECTED && (
+									<button
+										style={{ height: '60px' }}
+										className="btn btn-primary w-50 mt-3 mb-4 text-left"
+										onClick={() => {
+											connect(
+												'CHROME_EXTENSION'
+											)
+											//props.setShowDashboard(true)
+										}}
+									>
+										<img
+											src="./images/terra.svg"
+											width="30"
+											className="ml-4 mr-4"
+											alt="Terra"
+										/>
+										<span className="">
+											Connect Terra Station
+											Extension
+										</span>
+									</button>
+								)
+							)}
+						</div>
+					</div>
+
+					<div id="mobile-button-box" className="row">
+						<div className="col">
+							<button
+								style={{ height: '60px' }}
+								className="btn btn-primary w-50 text-left"
+								onClick={() => {
+									connect('WALLETCONNECT')
+								}}
+							>
+								<img
+									src="./images/walletconnect.svg"
+									width="30"
+									className="ml-4 mr-4"
+									alt="Wallet Connect"
+								/>
+								<span className="">
+									Connect Terra Station Mobile
+								</span>
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default LoginBox
