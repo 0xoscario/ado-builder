cf4194a48db5d7c92e8e05acde0dcce36b39e979
Jean-Michel Saulnier <jmsaulnier.dev@gmail.com>
2021-11-02 10:25:57 -0700

    Fix layout missions

--- src/pages/missions/index.tsx
+++ src/pages/missions/index.tsx
@@ -11,8 +11,8 @@ interface ItemProps {
 const Item: React.FC<ItemProps> = (prop: ItemProps) => {
   return (
     <div className="w-48 h-36 p-2.5 bg-white relative shadow-lg border-gray-300">
-      <h3 className="text-blue-900 text-xl font-medium">{prop.title}</h3>
-      <p className="text-blue-900 text-xs">{prop.uuid}</p>
+      <h3 className="text-blue-900 text-lg font-medium m-1">{prop.title}</h3>
+      <p className="text-blue-900 text-xs">UUID {prop.uuid}</p>
       {prop.icon && (
         <ArrowRightIcon className="w-8 text-gray-500 absolute top-1/2 -right-5 transform -translate-y-1/2" />
       )}
@@ -20,25 +20,25 @@ const Item: React.FC<ItemProps> = (prop: ItemProps) => {
   );
 };
 
-const itemData = [
+const itemData: ItemProps[] = [
   {
+    uuid: '009930',
     title: 'Accept Funds',
-    uuid: 'UUID 009930',
     icon: true,
   },
   {
+    uuid: '017830',
     title: 'Purchase Asset',
-    uuid: 'UUID 017830',
     icon: true,
   },
   {
+    uuid: '083130',
     title: 'Timelock',
-    uuid: 'UUID 083130',
     icon: true,
   },
   {
+    uuid: '049130',
     title: 'Splitter',
-    uuid: 'UUID 049130',
     icon: false,
   },
 ];
@@ -47,28 +47,32 @@ const Missions: NextPage = () => {
   return (
     <Layout title="My missions" currentNavIndex="2">
       <div className="flex mb-3 pb-3 border-b border-grey-500">
-        <h1 className="text-2xl font-medium text-gray-900">My Missions</h1>
+        <h1 className="text-2xl font-medium text-gray-700 m-3">My Missions</h1>
       </div>
-      <div className="text-center mt-12">
-        <h2 className="text-2xl font-medium text-gray-500">(In Development)</h2>
-        <p>
+      <div className="text-center mt-16">
+        <span className="text-2xl font-medium text-gray-500">
+          (In Development)
+        </span>
+        <p className="text-sm">
           Missions is currently under development, but below, you'll find an
           example of what they are and how they might be utilized.
         </p>
-        <div className="max-w-3xl mx-auto mt-6 mb-6">
-          <h2 className="text-red-500 font-medium">EXAMPLE MISSION</h2>
-          <p className="text-gray-500">
+        <div className="max-w-3xl mx-auto mt-8 mb-8">
+          <h2 className="text-red-500 font-medium uppercase p-2">
+            Example Mission
+          </h2>
+          <p className="text-gray-500 text-sm">
             An entity wants to accept funds and once a funding level is met,
             purchase an asset, hold the funds for 60 days, then split the funds
             among the depositers.
           </p>
-          <p className="mt-2 text-gray-500">
+          <p className="mt-2 text-gray-500 text-sm">
             Missions will allow chained ADOs to execute complex sets of events.
           </p>
         </div>
         <div className="flex justify-between items-center max-w-4xl mx-auto my-10">
           {itemData.map((x) => (
-            <Item title={x.title} uuid={x.uuid} icon={x.icon} />
+            <Item key={x.uuid} title={x.title} uuid={x.uuid} icon={x.icon} />
           ))}
         </div>
       </div>
