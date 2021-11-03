97cd07611e575bee83f52415a32f6eb1b4919f7f
0xBlockchain <buckoscarengr@gmail.com>
2021-11-03 14:11:13 -0400

    added a map component and updated builder page

--- src/pages/smart-contract-builder/index.tsx
+++ src/pages/smart-contract-builder/index.tsx
@@ -2,322 +2,164 @@ import type { NextPage } from 'next';
 import Link from 'next/link';
 import Layout from '@/components/DefaultLayout';
 
+interface BuilderItemProps {
+  svg: object;
+  title: string;
+  checkLists: [string, string, string, string];
+  url: string;
+}
+
+const BuilderItem: React.FC<BuilderItemProps> = (prop: BuilderItemProps) => {
+  return (
+    <div className="p-4 md:w-1/3">
+      <div className="flex rounded-lg h-full bg-white shadow sm:rounded-lg p-8 flex-col">
+        <div className="flex items-center mb-8">
+          <div className="w-8 h-8 mr-4 inline-flex items-center justify-center rounded-full bg-red-700 text-white flex-shrink-0">
+            {prop.svg}
+          </div>
+          <h2 className="text-gray-900 text-lg title-font font-medium">
+            {prop.title}
+          </h2>
+        </div>
+        <div className="flex-grow">
+          <nav className="flex flex-col sm:items-start sm:text-left text-center items-center -mb-1 space-y-2.5">
+            {prop.checkLists.map((x) => (
+              <a>
+                <span className="bg-green-100 text-green-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="3"
+                    className="w-3 h-3"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M20 6L9 17l-5-5"></path>
+                  </svg>
+                </span>
+                {x}
+              </a>
+            ))}
+          </nav>
+          <Link href={prop.url}>
+            <a className="content-end text-center flex  items-center mt-8 px-4 py-3 border border-transparent shadow-sm font-medium rounded-md text-white bg-gray-900 hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-700 sm:text-sm">
+              Get Started
+              <svg
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                className="w-4 h-4 ml-2"
+                viewBox="0 0 24 24"
+              >
+                <path d="M5 12h14M12 5l7 7-7 7"></path>
+              </svg>
+            </a>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const itemData: BuilderItemProps[] = [
+  {
+    svg: (
+      <svg
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        className="w-5 h-5"
+        viewBox="0 0 24 24"
+      >
+        <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+      </svg>
+    ),
+    title: 'NFT Collectible',
+    checkLists: [
+      'Add royalities',
+      'black/white list',
+      'taxes',
+      'robust metadata',
+    ],
+    url: '/smart-contract-builder/nft',
+  },
+  {
+    svg: (
+      <svg
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        className="w-5 h-5"
+        viewBox="0 0 24 24"
+      >
+        <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+        <circle cx="12" cy="7" r="4"></circle>
+      </svg>
+    ),
+    title: 'DeFi Instruments',
+    checkLists: [
+      'splitter',
+      'timelock',
+      'MIR & ANC',
+      '(more in dev)',
+    ],
+    url: '#',
+  },
+  {
+    svg: (
+      <svg
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        className="w-5 h-5"
+        viewBox="0 0 24 24"
+      >
+        <circle cx="6" cy="6" r="3"></circle>
+        <circle cx="6" cy="18" r="3"></circle>
+        <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
+      </svg>
+    ),
+    title: 'Generic ADO',
+    checkLists: [
+      'Address lists',
+      'Specific values',
+      'Primitive functions',
+      '(more in dev)',
+    ],
+    url: '#',
+  },
+];
+
 const SmartContractBuilder: NextPage = () => {
   return (
     <Layout title="Andromeda">
-      <section className="text-gray-600">
-        <div className="container mx-auto">
-          
-          <div className="flex flex-col text-center w-full mt-4 mb-8">
-            <h1 className="sm:text-2xl text-xl font-medium title-font text-gray-900 py-8">
-              NFT & Smart Contract Builder
-            </h1>
-          </div>
-{/*//////////////// This section needs to be build out with a map component load for each and all template types /////////////////*/}                
-          <div className="flex flex-wrap -m-4">
-            {/* NFT Collectible Template */}
-            <div className="p-4 md:w-1/3">
-              <div className="flex rounded-lg h-full bg-white shadow sm:rounded-lg p-8 flex-col">
-                <div className="flex items-center mb-8">
-                  <div className="w-8 h-8 mr-4 inline-flex items-center justify-center rounded-full bg-red-700 text-white flex-shrink-0">
-                    <svg
-                      fill="none"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      className="w-5 h-5"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-                    </svg>
-                  </div>
-                  <h2 className="text-gray-900 text-lg title-font font-medium">
-                    NFT Collectible
-                  </h2>
-                </div>
-                <div className="flex-grow">
-                  <nav className="flex flex-col sm:items-start sm:text-left text-center items-center -mb-1 space-y-2.5">
-                    <a>
-                      <span className="bg-green-100 text-green-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
-                        <svg
-                          fill="none"
-                          stroke="currentColor"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="3"
-                          className="w-3 h-3"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M20 6L9 17l-5-5"></path>
-                        </svg>
-                      </span>
-                      First Link
-                    </a>
-                    <a>
-                      <span className="bg-green-100 text-green-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
-                        <svg
-                          fill="none"
-                          stroke="currentColor"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="3"
-                          className="w-3 h-3"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M20 6L9 17l-5-5"></path>
-                        </svg>
-                      </span>
-                      Earn royalties
-                    </a>
-                    <a>
-                      <span className="bg-green-100 text-green-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
-                        <svg
-                          fill="none"
-                          stroke="currentColor"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="3"
-                          className="w-3 h-3"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M20 6L9 17l-5-5"></path>
-                        </svg>
-                      </span>
-                      Add an EULA
-                    </a>
-                    <a>
-                      <span className="bg-green-100 text-green-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
-                        <svg
-                          fill="none"
-                          stroke="currentColor"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="3"
-                          className="w-3 h-3"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M20 6L9 17l-5-5"></path>
-                        </svg>
-                      </span>
-                      Track transactions
-                    </a>
-                  </nav>
-                  <Link href="/smart-contract-builder/nft">
-                    <a className="content-end text-center flex  items-center mt-8 px-4 py-3 border border-transparent shadow-sm font-medium rounded-md text-white bg-gray-900 hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-700 sm:text-sm">
-                      Get Started
-                      <svg
-                        fill="none"
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        className="w-4 h-4 ml-2"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M5 12h14M12 5l7 7-7 7"></path>
-                      </svg>
-                    </a>
-                  </Link>
-                </div>
-              </div>
-            </div>
-
-            {/* DeFi Contract Template */}
-            <div className="p-4 md:w-1/3">
-              <div className="flex rounded-lg h-full bg-white shadow sm:rounded-lg p-8 flex-col">
-                <div className="flex items-center mb-8">
-                  <div className="w-8 h-8 mr-4 inline-flex items-center justify-center rounded-full bg-red-700 text-white flex-shrink-0">
-                    <svg
-                      fill="none"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      className="w-5 h-5"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
-                      <circle cx="12" cy="7" r="4"></circle>
-                    </svg>
-                  </div>
-                  <h2 className="text-gray-900 text-lg title-font font-medium">
-                    DeFi Contract Templates
-                  </h2>
-                </div>
-                <div className="flex-grow">
-                  <nav className="flex flex-col sm:items-start sm:text-left text-center items-center -mb-1 space-y-2.5">
-                    <a>
-                      <span className="bg-green-100 text-green-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
-                        <svg
-                          fill="none"
-                          stroke="currentColor"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="3"
-                          className="w-3 h-3"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M20 6L9 17l-5-5"></path>
-                        </svg>
-                      </span>
-                      Automate buy / sell
-                    </a>
-                    <a>
-                      <span className="bg-green-100 text-green-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
-                        <svg
-                          fill="none"
-                          stroke="currentColor"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="3"
-                          className="w-3 h-3"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M20 6L9 17l-5-5"></path>
-                        </svg>
-                      </span>
-                      Split funds / profits
-                    </a>
-                    <a>
-                      <span className="bg-green-100 text-green-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
-                        <svg
-                          fill="none"
-                          stroke="currentColor"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="3"
-                          className="w-3 h-3"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M20 6L9 17l-5-5"></path>
-                        </svg>
-                      </span>
-                      Tax Management
-                    </a>
-                    <a>
-                      <span className="bg-green-100 text-green-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
-                        <svg
-                          fill="none"
-                          stroke="currentColor"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="3"
-                          className="w-3 h-3"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M20 6L9 17l-5-5"></path>
-                        </svg>
-                      </span>
-                      Smart contracts
-                    </a>
-                  </nav>
-                  <a className="content-end text-center flex  items-center mt-8 px-4 py-3 border border-transparent shadow-sm font-medium rounded-md text-gray-400 bg-gray-200 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-100 sm:text-sm">
-                    Coming soon
-                  </a>
-                </div>
-              </div>
-            </div>
-
-            {/* Custom Contract Builder */}
-            <div className="p-4 md:w-1/3">
-              <div className="flex rounded-lg h-full bg-white shadow sm:rounded-lg p-8 flex-col">
-                <div className="flex items-center mb-8">
-                  <div className="w-8 h-8 mr-4 inline-flex items-center justify-center rounded-full bg-red-700 text-white flex-shrink-0">
-                    <svg
-                      fill="none"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      className="w-5 h-5"
-                      viewBox="0 0 24 24"
-                    >
-                      <circle cx="6" cy="6" r="3"></circle>
-                      <circle cx="6" cy="18" r="3"></circle>
-                      <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
-                    </svg>
-                  </div>
-                  <h2 className="text-gray-900 text-lg title-font font-medium">
-                    Assemble Custom Contract
-                  </h2>
-                </div>
-                <div className="flex-grow">
-                  <nav className="flex flex-col sm:items-start sm:text-left text-center items-center -mb-1 space-y-2.5">
-                    <a>
-                      <span className="bg-green-100 text-green-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
-                        <svg
-                          fill="none"
-                          stroke="currentColor"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="3"
-                          className="w-3 h-3"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M20 6L9 17l-5-5"></path>
-                        </svg>
-                      </span>
-                      Track transactions
-                    </a>
-                    <a>
-                      <span className="bg-green-100 text-green-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
-                        <svg
-                          fill="none"
-                          stroke="currentColor"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="3"
-                          className="w-3 h-3"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M20 6L9 17l-5-5"></path>
-                        </svg>
-                      </span>
-                      Collect funds
-                    </a>
-                    <a>
-                      <span className="bg-green-100 text-green-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
-                        <svg
-                          fill="none"
-                          stroke="currentColor"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="3"
-                          className="w-3 h-3"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M20 6L9 17l-5-5"></path>
-                        </svg>
-                      </span>
-                      Document signing
-                    </a>
-                    <a>
-                      <span className="bg-green-100 text-green-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
-                        <svg
-                          fill="none"
-                          stroke="currentColor"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="3"
-                          className="w-3 h-3"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M20 6L9 17l-5-5"></path>
-                        </svg>
-                      </span>
-                      Cap tables
-                    </a>
-                  </nav>
-                  <a className="content-end text-center flex  items-center mt-8 px-4 py-3 border border-transparent shadow-sm font-medium rounded-md text-gray-400 bg-gray-200 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-100 sm:text-sm">
-                    Coming soon
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
+      <div className="text-gray-600 max-w-4xl container mx-auto">
+        <div className="flex flex-col text-center w-full mt-4 mb-8">
+          <h1 className="sm:text-2xl text-xl font-medium title-font text-gray-900">
+            ADO & NFT Builder
+          </h1>
+          <p>Create NFT collectibles, DeFi instruments and generic ADOs</p>
         </div>
-      </section>
+        <div className="flex flex-wrap -m-4">
+          {itemData.map((x) => (
+            <BuilderItem
+              svg={x.svg}
+              title={x.title}
+              checkLists={x.checkLists}
+              url={x.url}
+            />
+          ))}
+        </div>
+      </div>
     </Layout>
   );
 };
